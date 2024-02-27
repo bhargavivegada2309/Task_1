ba0927a61016fe6d149843c3e65cea110f5b4e23
bhargavivegada2309 <155133149+bhargavivegada2309@users.noreply.github.com>
2024-02-27 09:08:49 +0530

    array

---
+++ array2.php
@@ -0,0 +1,6 @@
+<?php
+$student=array('Name'=>'priya','sarname'=>'prasad','class'=>'b2','rollno'=>'28','Enroll_no'=>'220801245','name_of_uni'=>'Atmiya','uni'=>'University','sub1'=>'java','sub2'=>'php','sub3'=>'ds',);
+echo"<br>";
+print_r($student);
+
+?>
---
+++ array3.php
@@ -0,0 +1,9 @@
+<?php
+$student_master=array(array("name"=>"priya"),array("Surname"=>"prasad"),array(array("Division"=>"b2"),array("rollno"=>"28"),array("Enroll_no"=>"220801245"),array("name_of_uni"=>"Atmiya"),array("uni"=>"University"),array("sub1"=>"java"),array("sub2"=>"php"),array("sub3"=>"ds"));
+foreach($student_master as $mytemp)
+{
+	print_r($mytemp);
+	echo"<br>";
+}
+
+?>
